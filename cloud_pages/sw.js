b7c25d10cd306d87d147e5102702c76b05a58f1b
Chicken3veryDay <78713149+Chicken3veryDay@users.noreply.github.com>
2026-07-14 18:03:27 -0400

    ui: invalidate stale cached catalog shell

--- cloud_pages/sw.js
+++ cloud_pages/sw.js
@@ -1,4 +1,4 @@
-const CACHE='dropfinder-cloud-v2';
+const CACHE='dropfinder-cloud-v3';
 const SHELL=['./','index.html','manifest.webmanifest','icon.svg','data/catalog.json','data/status.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
